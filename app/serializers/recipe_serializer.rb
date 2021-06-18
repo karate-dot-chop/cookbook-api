8096444f054bfe3161638001c56e9dc9878f1464
dani <dani.zaghian@gmail.com>
2021-06-18 11:50:51 -0700

    adds recent changes

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -2,4 +2,11 @@ class RecipeSerializer < ActiveModel::Serializer
   attributes :id, :title, :ingredients, :directions, :image_url, :prep_time, :created_at
 
   belongs_to :user
+  
+  attribute :owner, if: :current_user
+
+  def owner
+    object.user_id == current_user.id
+  end
+
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email
+
+  # has_many :recipes, key: :last_recipe do
+  #   object.recipes.last
+  # end
+
+  has_many :recipes, if: :current_user
 end
